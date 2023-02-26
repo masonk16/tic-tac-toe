2e887dea81c0ce846b923f8b8ce6eb5f69454713
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-26 21:22:27 +0200

    Create constructor class for Game object.

---
+++ game.py
@@ -0,0 +1,20 @@
+from players import Player, Move
+from itertools import cycle
+
+BOARD_SIZE = 3
+DEFAULT_PLAYERS = (
+    Player(label="X", color="blue"),
+    Player(label="O", color="green"),
+)
+
+
+class TicTacToeGame:
+    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+        self._players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self._players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_combos = []
+        self._setup_board()
