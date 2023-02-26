de077689bbb74529e33434ca0a8575b53a4852be
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-26 21:31:06 +0200

    Create main function to run game.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 """A tic-tac-toe game built with Python and Tkinter."""
+from game import TicTacToeGame
+from board import TicTacToeBoard
+
+
+def main():
+    """Create the game's board and run its main loop."""
+    game = TicTacToeGame()
+    board = TicTacToeBoard(game)
+    board.mainloop()
+
+
+if __name__ == "__main__":
+    main()
